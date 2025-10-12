054ec4613df6e5ae96030a9676ec5af1d2f31856
thomve <thomas.veran@gmail.com>
2025-10-12 20:44:20 +0200

    string scrambling

---
+++ graph/string_scrambled.py
@@ -0,0 +1,35 @@
+def isScramble(self, s1: str, s2: str) -> bool:
+    if len(s1) != len(s2):
+        return False
+    if s1 == s2:
+        return True
+    if sorted(s1) != sorted(s2):
+        return False
+
+    memo = {}
+
+    def dfs(a, b):
+        if (a, b) in memo:
+            return memo[(a, b)]
+        if a == b:
+            memo[(a, b)] = True
+            return True
+        if sorted(a) != sorted(b):
+            memo[(a, b)] = False
+            return False
+
+        n = len(a)
+        for i in range(1, n):
+            # Case 1: No swap
+            if dfs(a[:i], b[:i]) and dfs(a[i:], b[i:]):
+                memo[(a, b)] = True
+                return True
+            # Case 2: Swap
+            if dfs(a[:i], b[-i:]) and dfs(a[i:], b[:-i]):
+                memo[(a, b)] = True
+                return True
+
+        memo[(a, b)] = False
+        return False
+
+    return dfs(s1, s2)
